004c42dbbb4db4c97160587e3e9e9312d2c635d0
emilio lopez <emailemilioee@gmail.com>
2025-03-31 10:59:33 -0700

    test javafx application

--- creaturekeeper/src/main/java/com/creaturekeeper/app/App.java
+++ creaturekeeper/src/main/java/com/creaturekeeper/app/App.java
@@ -1,10 +1,22 @@
 package com.creaturekeeper.app;
 
-/**
- * Hello world!
- */
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setTitle("JavaFX with Maven");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 }
